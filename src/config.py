b65ac9d189e44f9fafe0bf7552b8b569e7b0dfec
unnhao <unnhao@gmail.com>
2019-10-03 16:22:04 +0800

    :tada: Initial commit

---
+++ src/config.py
@@ -0,0 +1,32 @@
+# /src/config.py
+
+import os
+
+class Development(object):
+    """
+    Development environment configuration
+    """
+    DEBUG = True
+    TESTING = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class Production(object):
+    """
+    Production environment configurations
+    """
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    
+
+app_config = {
+    'development': Development,
+    'production': Production,
+}
+
+# $Env:FLASK_ENV="development"
+# $Env:DATABASE_URL= "sqlite:////tmp/test87.db"
+# $Env:JWT_SECRET_KEY = "87"
